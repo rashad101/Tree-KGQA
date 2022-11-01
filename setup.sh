a3b47a0391b36827d31918408df36bc9de4ce624
rashad101 <rah.rony@gmail.com>
2022-11-01 11:38:46 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ mv entity.jsonl data/
 mkdir outputs/EL
 mkdir outputs/ablation
 mkdir outputs/kgqa
+wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_300d.txt.bz2
+bzip2 -d enwiki_20180420_300d.txt.bz2
+mv enwiki_20180420_300d.txt data/
