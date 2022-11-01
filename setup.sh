0a51a92b0e5e7dd046b8518397dfd87555564778
rashad101 <rah.rony@gmail.com>
2022-11-01 12:59:55 +0100

    modified README.md

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ rm wiki.simple.vec wiki.simple.zip
 
 wget http://dl.fbaipublicfiles.com/elq/entity.jsonl
 mv entity.jsonl data/
+
+
 mkdir outputs/EL
 mkdir outputs/ablation
 mkdir outputs/kgqa
+
+
 wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_300d.txt.bz2
 bzip2 -d enwiki_20180420_300d.txt.bz2
 mv enwiki_20180420_300d.txt data/
